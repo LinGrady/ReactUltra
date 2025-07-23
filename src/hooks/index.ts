d9a0d7cee87127179da3ed5063b4fc8d3b9282dd
grady <2789975504@qq.com>
2025-07-23 14:59:14 +0800

    feat: 添加index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,26 @@
+// 现有的Hook
+export * from './use-mobile';
+export * from './useApi';
+export * from './useAuth';
+export * from './useCodeSplitting';
+export * from './useDebounceThrottle';
+export * from './useIntersectionObserver';
+export * from './useLocalStorage';
+export * from './useNetworkStatus';
+export * from './usePerformanceOptimization';
+export * from './useTheme';
+export * from './useTranslation';
+export * from './useUserBehavior';
+
+// 新增的实用Hook
+export * from './useClipboard';
+export * from './useMediaQuery';
+export * from './useClickOutside';
+export * from './useWindowSize';
+export * from './useKeyboard';
+export * from './useToggle';
+export * from './usePrevious';
+export * from './useIdle';
+export * from './useTimer';
+export * from './usePermission';
+export * from './useGeolocation'; 
